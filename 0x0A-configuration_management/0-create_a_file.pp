2d5cc7e55fc566c8c4128f1630b8c72bff1e3324
luischaparoc <lfchaparro@outlook.com>
2019-12-09 19:19:54 +0000

    [Feature]: adding a manifest to create a file in /tmp New file: 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
