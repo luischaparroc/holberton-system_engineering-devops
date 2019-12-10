3562c3372b4f6c254298c55922f412b3f4757132
luischaparoc <lfchaparro@outlook.com>
2019-12-10 03:55:33 +0000

    [Fix]: adding comment Modified: 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# Changes SSH config file
 exec { 'echo':
   path    => 'usr/bin:/bin',
   command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
