8779f29093b24fb2599342599a4adc14319a05d5
luischaparoc <lfchaparro@outlook.com>
2019-12-11 23:05:58 +0000

    Fix 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,11 @@
 # Installs a Nginx server
 exec { 'install nginx':
-  path     => 'usr/bin:/bin',
   command  => 'apt-get -y update; apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.nginx-debian.html',
   provider => shell,
   returns  => [0,1],
 }
 
 exec { 'replace':
-  path     => 'usr/bin:/bin',
   command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default; service nginx start',
   provider => shell,
   returns  => [0,1],
