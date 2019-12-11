b734f3710175ddd57895635b700e44fcc2edc194
luischaparoc <lfchaparro@outlook.com>
2019-12-11 22:52:21 +0000

    Adding end line

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-# Changes SSH config file
+# Installs a Nginx server
 exec { 'echo':
   path    => 'usr/bin:/bin',
   command => 'apt-get -y update; apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.nginx-debian.html; sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default; service nginx start',
