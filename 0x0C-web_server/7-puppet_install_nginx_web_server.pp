caffed39df927b0a949d2a6ee1d8774f60d50760
luischaparoc <lfchaparro@outlook.com>
2019-12-12 04:07:47 +0000

    Reducing lines on task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,6 @@ exec {'install':
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
   provider => shell,
 }
-
 exec {'replace':
   command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
   provider => shell,
