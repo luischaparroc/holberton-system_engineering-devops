479caeff13031d69f073a64675ec04ffb6be0a64
luischaparroc <lfchaparro@outlook.com>
2020-01-02 20:19:37 +0000

    [Feature]: adding Puppet to customize HTTP header in a server New file: 2-puppet_custom_http_response-header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,24 @@
+# Installs a Nginx server with custome HTTP header
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update'
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx'
+  provider => shell,
+  command => 'sudo apt-get -y install nginx',
+  before => Exec['add_header'],
+}
+
+exec { 'add_header'
+  provider => shell,
+  command => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
+  before => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx'
+  provider => shell,
+  command => 'sudo service nginx restart',
+}
