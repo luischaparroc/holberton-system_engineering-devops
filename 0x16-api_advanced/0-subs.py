f5bd33d1b4ab572ddec89a5e9f5f6c0deeb0159d
luischaparroc <lfchaparro@outlook.com>
2020-02-06 09:37:11 -0500

    [Feature]: adding function that returns the num of subscribers New file: 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers)
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
