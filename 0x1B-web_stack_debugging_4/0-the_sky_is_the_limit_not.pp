3f56ebfef702841629b9d98a86ed67b1bc06ba87
luischaparroc <lfchaparro@outlook.com>
2020-02-25 22:44:04 -0500

    [Feature]: resource that fix the problem of high amount of requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
