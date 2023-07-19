27d0b1679c4f57b9e70770dc416abb3ecb8d4543
purple360 <64763854+purple360@users.noreply.github.com>
2023-07-19 12:43:53 +0000

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Changes SSH config file
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0,1],
 }
